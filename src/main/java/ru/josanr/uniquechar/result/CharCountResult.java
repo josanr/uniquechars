b2dfe136d80c409c7d137e7e394ea87a4501ad4a
ruslan <josanr@gmail.com>
2021-11-24 14:55:13 +0300

    Task5: format print

--- src/main/java/ru/josanr/uniquechar/result/CharCountResult.java
+++ src/main/java/ru/josanr/uniquechar/result/CharCountResult.java
@@ -3,4 +3,18 @@ package ru.josanr.uniquechar.result;
 import java.util.HashMap;
 
 public class CharCountResult extends HashMap<String, Integer> {
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (var entry : this.entrySet()) {
+            result.append("\"")
+                .append(entry.getKey())
+                .append("\"")
+                .append(" - ")
+                .append(entry.getValue())
+                .append("\n");
+        }
+
+        return result.toString();
+    }
 }
